587a52d54bbe4787565bf3c3397beb90b84786ba
utkanbasurgan <utkan@basurgan.com>
2025-05-09 07:19:19 +0300

    Search eklendi.

---
+++ src/main/java/com/sas/social/dto/SearchResultDto.java
@@ -0,0 +1,53 @@
+package com.sas.social.dto;
+
+/**
+ * DTO for search results that can contain users, communities, or posts
+ */
+public class SearchResultDto {
+    
+    private String type; // USER, COMMUNITY, or POST
+    private Long id;
+    private String name;
+    private String description;
+    private Object data; // The full DTO for the specific entity
+    
+    public String getType() {
+        return type;
+    }
+    
+    public void setType(String type) {
+        this.type = type;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+    
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    public Object getData() {
+        return data;
+    }
+    
+    public void setData(Object data) {
+        this.data = data;
+    }
+}
